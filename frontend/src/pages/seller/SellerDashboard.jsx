883769854df4ce120e9b0940c3fa7aa93af13e07
Yasv#20041 <yasvathika.21it@kongu.edu>
2023-11-19 14:56:28 +0530

    initial commit

--- frontend/src/pages/seller/SellerDashboard.jsx
+++ frontend/src/pages/seller/SellerDashboard.jsx
@@ -91,7 +91,7 @@ const SellerDashboard = () => {
                             >
                                 <LocalMallIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
 
-                                SHOPCART
+                                PLANTSTORE
                             </NavLogo>
                         </Typography>
 
